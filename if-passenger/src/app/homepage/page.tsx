36a54ebb32dcb07b685647d253cbf6a3e9c7d558
OtavioMaldaner <otaviomaldaner571@gmail.com>
2024-03-24 21:08:14 -0300

    upload

--- if-passenger/src/app/homepage/page.tsx
+++ if-passenger/src/app/homepage/page.tsx
@@ -1,10 +1,65 @@
+"use client";
 import Header from "@/components/default/Header";
-import { Select } from "@/components/ui/select";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { SelectItem } from "@radix-ui/react-select";
+import { jwtDecode } from "jwt-decode";
+import { cookies } from "next/headers";
+import { ChangeEvent, useState } from "react";
+import { getDecodedToken } from "../api/functions";
+import { JWTToken } from "../api/types";
 
 export default function Homepage() {
+  const token: JWTToken = getDecodedToken();
+
+  const [selected, setSelected] = useState<string>("following");
+
+  const values: { value: string; name: string }[] = [
+    {
+      value: "following",
+      name: "Seguindo",
+    },
+    {
+      value: "city",
+      name: token.city,
+    },
+    {
+      value: "course",
+      name: token.course,
+    },
+    {
+      value: "in2hours",
+      name: "Em 2 horas",
+    },
+  ];
   return (
     <main>
-      <Header></Header>
+      <Header>
+        <Select defaultValue="following" onValueChange={(e) => setSelected(e)}>
+          <SelectTrigger className="w-[140px] border-0 focus:outline-none">
+            <SelectValue>
+              {values.find((value) => value.value == selected)?.name ||
+                "Seguindo"}
+            </SelectValue>
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              {values.map((value) => {
+                return (
+                  <SelectItem key={value.value} value={value.value}>
+                    {value.name}
+                  </SelectItem>
+                );
+              })}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </Header>
     </main>
   );
 }
